e96a24701729af578b8b13af982a0e981a2aea06
subhash dangi <166481929+Subhashdangi205@users.noreply.github.com>
2025-01-20 21:22:04 +0530

    Create Digital clock.js

---
+++ Digital clock.js
@@ -0,0 +1,11 @@
+function Clock(){
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    hours = hours < 10 ? "0" + hours : hours
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    seconds = seconds < 10 ? "0" + seconds : seconds
+    let time = hours + ":" + minutes + ":" + seconds
+    document.getElementById("clock").innerHTML = time;
+}
